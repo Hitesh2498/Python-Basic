504c24434fe46ee284533bb2c211767b36f31de9
Hitesh Sharma <96277290+Hitesh2498@users.noreply.github.com>
2024-06-09 16:52:04 +0530

    Add files via upload

---
+++ Hitesh.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Welcome to my code")
+
+# if function is excetuded from here then run the function
+# if this script is imported to another program then dont run the below function
+if __name__ == "__main__":
+    welcome()
